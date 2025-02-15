b6b3f9f90478ffcfc2460b47d23b980523763ed3
gabai62 <gabai62it@gmail.com>
2025-02-15 16:37:58 +0200

    make build

---
+++ index.9446e56c.js
@@ -0,0 +1,2 @@
+!function e(n,t){if(Object.keys(t).length){var r=document.createElement("ul");for(var o in t){var l,a=document.createElement("li");a.textContent=o,"object"==((l=t[o])&&"undefined"!=typeof Symbol&&l.constructor===Symbol?"symbol":typeof l)&&null!==t[o]&&e(a,t[o]),r.append(a)}n.append(r)}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.9446e56c.js.map
